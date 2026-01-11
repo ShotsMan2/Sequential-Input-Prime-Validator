905e1f3f2191d46aea761751045f89f232baae46
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 10:55:08 +0300

    Implement dynamic input polling with prime detection
    Added source code that continuously polls for user input and halts the execution loop when the input satisfies primality conditions.

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi,i,j,k,asal;
+
+    for(i=1;i<1000;i++){
+        printf("%d. sayiyi girin",i);
+        scanf("%d",&sayi);
+
+        for(j=2;j<sayi;j++){
+            asal=1;
+            for(k=2;k<=j/2;k++){
+                if(j%k==0){
+                    asal=0;
+                    break;
+                }
+            }
+            if(asal==1){
+
+            }
+        }
+        if(asal==1){
+            break;
+        }
+
+
+
+    }
+    printf("%d adet sayi girdiniz",i);
+}
