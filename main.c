7befe3923ec6ee065f40e3ad40ceeeb642147226
Selim Efe <uysalselimefe@gmail.com>
2026-01-19 15:46:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,30 +3,32 @@
 
 int main()
 {
-    int sayi,i,j,k,asal;
+    int sayi, i, j, k, asal;
 
-    for(i=1;i<1000;i++){
-        printf("%d. sayiyi girin",i);
-        scanf("%d",&sayi);
+    for (i = 1; i < 1000; i++)
+    {
+        printf("%d. sayiyi girin", i);
+        scanf("%d", &sayi);
 
-        for(j=2;j<sayi;j++){
-            asal=1;
-            for(k=2;k<=j/2;k++){
-                if(j%k==0){
-                    asal=0;
+        for (j = 2; j < sayi; j++)
+        {
+            asal = 1;
+            for (k = 2; k <= j / 2; k++)
+            {
+                if (j % k == 0)
+                {
+                    asal = 0;
                     break;
                 }
             }
-            if(asal==1){
-
+            if (asal == 1)
+            {
             }
         }
-        if(asal==1){
+        if (asal == 1)
+        {
             break;
         }
-
-
-
     }
-    printf("%d adet sayi girdiniz",i);
+    printf("%d adet sayi girdiniz", i);
 }
